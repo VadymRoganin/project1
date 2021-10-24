e6b89f7aaa17e02ce83b8425aec01a681eac4030
Vadim Roganin <vadym.roganin@gmail.com>
2021-10-24 14:35:32 +0300

    JIRA-001: Changed the formatting

--- src/com/start/ArrayPermutation.java
+++ src/com/start/ArrayPermutation.java
@@ -3,16 +3,19 @@ package com.start;
 import java.util.Arrays;
 
 public class ArrayPermutation {
+
     static void permutationWithValue(int[] array, int i, int j) {
         int temp = array[i];
         array[i] = array[j];
         array[j] = temp;
     }
+
     static void permutationWithoutValue(int[] array, int i, int j) {
         array[i] = array[i] + array[j];
-        array[j] = array[i]  - array[j];
+        array[j] = array[i] - array[j];
         array[i] = array[i] - array[j];
     }
+
     public static void main(String[] args) {
         int [] array = {1, 2, 3, 4};
         System.out.print(Arrays.toString(array));
@@ -22,6 +25,5 @@ public class ArrayPermutation {
         System.out.println();
         permutationWithoutValue(array, 3, 1);
         System.out.println(Arrays.toString(array));
-
     }
 }
